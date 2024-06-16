3f6270d060cc76b77acf1aebf57265f9e6d8029e
DimensionSTP <ddang8jh@gmail.com>
2024-06-17 07:17:11 +0900

    add: basic preprocessing scripts

---
+++ scripts/preprocess.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+path="src/preprocessing"
+
+python $path/add_empty_column.py
+python $path/make_corpus.py
+python $path/train_tokenizer.py
+python $path/merge_tokenizer.py
+python $path/merge_model.py
+
+dataset_modes="train test"
+
+for dataset_mode in $dataset_modes
+do
+    python $path/preprocess_dataset.py mode=$dataset_mode
+done
---
+++ scripts/preprocess_dataset.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+path="src/preprocessing"
+upload_users=("meta-llama" "meta-llama" "KevSun" "microsoft")
+model_types=("Meta-Llama-3-8B-Instruct" "Meta-Llama-3-8B" "IELTS_essay_scoring" "deberta-v3-large")
+length=${#model_types[@]}
+dataset_modes="train test"
+
+for ((i=0; i<$length; i++))
+do
+    upload_user=${upload_users[$i]}
+    model_type=${model_types[$i]}
+    python $path/merge_tokenizer.py upload_user=$upload_user model_type=$model_type
+    for dataset_mode in $dataset_modes
+    do
+        python $path/preprocess_dataset.py mode=$dataset_mode upload_user=$upload_user model_type=$model_type
+    done
+done
