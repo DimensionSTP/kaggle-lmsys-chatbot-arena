66779bf344adfc2820309cb03ee9a0db52e85a0c
DimensionSTP <ddang8jh@gmail.com>
2024-06-17 07:16:54 +0900

    feat: args

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
+is_causal=True
+is_preprocessed=True
 is_tuned="untuned"
-strategy="ddp"
-upload_user="microsoft"
-model_type="swinv2-large-patch4-window12to16-192to256-22kto1k-ft"
-precision=32
+strategy="deepspeed_stage_3_offload"
+upload_user="meta-llama"
+model_type="Meta-Llama-3-8B-Instruct"
+quantization_type="quantization"
+peft_type="lora"
+data_max_length=1024
+target_max_length=256
+precision="bf16"
 batch_size=24
-epochs="9 10"
+epochs="3 4"
 
 for epoch in $epochs
 do
     python main.py mode=predict \
+        is_causal=$is_causal \
+        is_preprocessed=$is_preprocessed \
         is_tuned=$is_tuned \
         strategy=$strategy \
         upload_user=$upload_user \
         model_type=$model_type \
+        quantization_type=$quantization_type \
+        peft_type=$peft_type \
+        data_max_length=$data_max_length \
+        target_max_length=$target_max_length \
         precision=$precision \
         batch_size=$batch_size \
         epoch=$epoch
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+is_causal=True
+is_preprocessed=True
 is_tuned="untuned"
-strategy="ddp"
-upload_user="microsoft"
-model_type="swinv2-large-patch4-window12to16-192to256-22kto1k-ft"
-precision=32
+strategy="deepspeed_stage_3_offload"
+upload_user="meta-llama"
+model_type="Meta-Llama-3-8B-Instruct"
+quantization_type="quantization"
+peft_type="lora"
+data_max_length=1024
+target_max_length=256
+precision="bf16"
 batch_size=24
 
 python main.py mode=train \
+    is_causal=$is_causal \
+    is_preprocessed=$is_preprocessed \
     is_tuned=$is_tuned \
     strategy=$strategy \
     upload_user=$upload_user \
     model_type=$model_type \
+    quantization_type=$quantization_type \
+    peft_type=$peft_type \
+    data_max_length=$data_max_length \
+    target_max_length=$target_max_length \
     precision=$precision \
     batch_size=$batch_size
