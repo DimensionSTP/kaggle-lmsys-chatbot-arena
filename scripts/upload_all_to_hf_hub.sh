ec66e7419ad60dbc12c8bb44f22fb2856b7dd2c7
DimensionSTP <ddang8jh@gmail.com>
2024-12-24 18:37:07 +0900

    add: prepare and upload all scripts

---
+++ scripts/upload_all_to_hf_hub.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+path="src/postprocessing"
+is_preprocessed=False
+is_tuned="untuned"
+strategy="deepspeed_stage_3_offload"
+upload_user="meta-llama"
+model_type="Meta-Llama-3-8B-Instruct"
+left_padding=False
+quantization_type="origin"
+peft_type="origin"
+data_max_length=1022
+target_max_length=2
+precision="bf16"
+batch_size=96
+model_detail="Llama-3-8B-Instruct"
+
+python $path/upload_all_to_hf_hub.py \
+    is_preprocessed=$is_preprocessed \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    left_padding=$left_padding \
+    quantization_type=$quantization_type \
+    peft_type=$peft_type \
+    data_max_length=$data_max_length \
+    target_max_length=$target_max_length \
+    precision=$precision \
+    batch_size=$batch_size \
+    model_detail=$model_detail
